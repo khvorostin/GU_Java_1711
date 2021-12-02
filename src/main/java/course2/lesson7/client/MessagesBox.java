fb59c92b4c5840536f9dcb9524f03d22e7465253
Denis Khvorostin <denis.khvorostin@gmail.com>
2021-12-02 22:05:37 +0300

    Java. Уровень 2. Урок 7, домашнее задание (приватные сообщения)

---
+++ src/main/java/course2/lesson7/client/MessagesBox.java
@@ -0,0 +1,49 @@
+package course2.lesson7.client;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class MessagesBox {
+
+    private List<Message> messages;
+
+    public class Message {
+
+        private String author;
+        private String message;
+
+        public Message(String author, String message) {
+            this.author = author;
+            this.message = message;
+        }
+
+        public String getAuthor() {
+            return author;
+        }
+
+        public String getMessage() {
+            return message;
+        }
+
+        @Override
+        public String toString() {
+            return "{" + author + ": " + message + "}";
+        }
+    }
+
+    public MessagesBox() {
+        this.messages = new LinkedList<>();
+    }
+
+    public void addMessage(String from, String text) {
+        messages.add(new Message(from, text));
+    }
+
+    public List<Message> getMessages() {
+        return messages;
+    }
+
+    public void clearAll() {
+        messages.clear();
+    }
+}
