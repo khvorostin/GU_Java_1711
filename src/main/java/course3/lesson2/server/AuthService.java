d030d45b196032edea76c717dee5afd17286803d
Denis Khvorostin <denis.khvorostin@gmail.com>
2021-12-16 13:35:24 +0300

    Move code from previous lesson

---
+++ src/main/java/course3/lesson2/server/AuthService.java
@@ -0,0 +1,10 @@
+package course3.lesson2.server;
+
+public interface AuthService {
+
+    void start();
+
+    String getNickByLoginPass(String login, String pass);
+
+    void stop();
+}
---
+++ src/main/java/course3/lesson2/server/ServerApp.java
@@ -0,0 +1,8 @@
+package course3.lesson2.server;
+
+public class ServerApp {
+
+    public static void main(String[] args) {
+        new MyServer();
+    }
+}
