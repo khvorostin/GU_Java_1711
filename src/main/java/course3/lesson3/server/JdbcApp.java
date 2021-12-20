c113a65bd9f1d5c5bc320b8e3ca6c9e7525c5686
Denis Khvorostin <denis.khvorostin@gmail.com>
2021-12-20 18:53:09 +0300

    Java. Уровень 3. Урок 3, домашнее задание (потоки ввода-вывода)

---
+++ src/main/java/course3/lesson3/server/JdbcApp.java
@@ -0,0 +1,57 @@
+package course3.lesson3.server;
+
+import java.sql.*;
+
+public class JdbcApp {
+
+    private static Connection connection;
+    private static Statement stmt;
+
+    public JdbcApp() {
+        try {
+            Class.forName("org.sqlite.JDBC");
+            connect();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            disconnect();
+        }
+    }
+
+    public void connect() throws SQLException {
+        connection = DriverManager.getConnection("jdbc:sqlite:javachat.db");
+        stmt = connection.createStatement();
+    }
+
+    public void disconnect() {
+        try {
+            if (stmt != null) {
+                stmt.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            if (connection != null) {
+                connection.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String getNickByLoginPass(String login, String pass) throws SQLException {
+        String nick = null;
+        PreparedStatement ps = connection.prepareStatement("SELECT username FROM users WHERE login = ? AND pass = ?");
+        ps.setString(1, login);
+        ps.setString(2, pass);
+        try (ResultSet rs = ps.executeQuery()) {
+            while (rs.next()) {
+                nick = rs.getString("username");
+            }
+        }
+
+        return nick;
+    }
+}
